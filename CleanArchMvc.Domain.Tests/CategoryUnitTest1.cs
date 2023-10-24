22979b4109e4dda8f24d585e04754e6cf0daed53
Gabriel Henrique <gabrielhnespindola@gmail.com>
2023-10-23 23:58:28 -0300

    Init

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,51 @@
+using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact(DisplayName = "Create Category with valid state")]
+    public void CreateCategory_WithValidParameters_ResultObjectValidState()
+    {
+        Action action = () => new Category(1, "Category Name");
+        action.Should()
+            .NotThrow<Validation.DomainExceptionValidation>();
+    }
+
+    [Fact]
+    public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+    {
+        Action action = () => new Category(-1, "Category Name");
+        action.Should()
+            .Throw<Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid id value. Id most be greater than 0");
+    }
+
+    [Fact]
+    public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+    {
+        Action action = () => new Category(1, "Ca");
+        action.Should()
+            .Throw<Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid name. Name is too short, minimum 3 characters");
+    }
+
+    [Fact]
+    public void CreateCategory_NullNameValue_DomainExceptionNullName()
+    {
+        Action action = () => new Category(1, null);
+        action.Should()
+            .Throw<Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid Name. Name is required");
+    }
+
+    [Fact]
+    public void CreateCategory_EmptyNameValue_DomainExceptionEmptyName()
+    {
+        Action action = () => new Category(1, "");
+        action.Should()
+            .Throw<Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid Name. Name is required");
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities;
+
+// Sealed -> classe não pode ser herdada
+public sealed class Category : Entity
+{    
+    public string Name { get; private set; }
+
+    public ICollection<Product> Products { get; private set; }
+
+    public Category(string name)
+    {
+        ValidateDomain(name);
+    }
+
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid id value. Id most be greater than 0");
+        Id = id;
+        ValidateDomain(name);
+    }
+
+    public void Update(string name){
+        ValidateDomain(name);
+    }
+
+    private void ValidateDomain(string name){
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid Name. Name is required");
+        DomainExceptionValidation.When(name.Length < 3, "Invalid name. Name is too short, minimum 3 characters");
+
+        Name = name;
+    }
+}
