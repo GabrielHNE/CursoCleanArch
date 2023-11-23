6257c4cf526b7371d757f8acceab18c6341b22b0
GabrielHNE <gabrielhnespindola@gmail.com>
2023-11-23 10:45:03 -0300

    Implementação da camada de Infra. Criacao dos repositories

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Infra.Data.EntitiesConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options)
+    {}
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        /*
+            Busca os arquivos que implementaram o 
+            IEntityTypeConfiguration e os insere manualmente
+        */
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        /*Sem o ApplyConfiguration, poderiamos inserir cada configuracao manualmente*/
+        // builder.ApplyConfiguration(new CategoryConfiguration());
+    }
+}
