a4d11b6baf0d0561556c8fe6b4c4f3b86f277862
Gabriel Henrique <gabrielhnespindola@gmail.com>
2024-05-24 00:32:48 -0300

    Criacao das views e dos métodos para produtos e categorias. Finalizacao do webUI

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -28,6 +30,72 @@ namespace CleanArchMvc.WebUI.Controllers
         public IActionResult Error()
         {
             return View("Error!");
+        }
+
+        [HttpGet()]
+        public IActionResult Create(){
+            return View();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Create(CategoryDTO cat){
+            if(ModelState.IsValid)
+            {
+                await _catService.Add(cat);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(cat);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id){
+            if(id == null) return NotFound();
+
+            var catDTO = await _catService.GetById(id);
+            return catDTO != null? View(catDTO) : NotFound();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(CategoryDTO cat){
+            if(ModelState.IsValid) {
+                try {
+                    await _catService.Update(cat);
+                }
+                catch(Exception ex) {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(cat);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id){
+            if(id == null) return NotFound();
+
+            var catDTO = await _catService.GetById(id);
+            return catDTO != null? View(catDTO) : NotFound();
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id) {
+            await _catService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id){
+            if(id == null)
+                return NotFound();
+
+            var catDTO = await _catService.GetById(id);
+
+            if(catDTO == null)
+                return NotFound();
+
+            return View(catDTO);
         }
     }
 }
--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -12,9 +15,13 @@ namespace CleanArchMvc.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _prodService;
-        public ProductsController(IProductService prodService)
+        private readonly ICategoryService _catService;
+        private readonly IWebHostEnvironment _env;
+        public ProductsController(IWebHostEnvironment env, IProductService prodService, ICategoryService catService)
         {
+            _env = env;
             _prodService = prodService;
+            _catService = catService;
         }
 
         [HttpGet]
@@ -22,6 +29,91 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var products = await _prodService.GetProducts();
             return View(products);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create(){
+            ViewBag.Categories = new 
+                SelectList(await _catService.GetCategories(), "Id", "Name");
+            
+            return View();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Create(ProductDTO prod){
+            Console.WriteLine(prod.CategoryId);
+            if(ModelState.IsValid){
+                await _prodService.Add(prod);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Categories = new 
+                SelectList(await _catService.GetCategories(), "Id", "Name");
+            return View(prod);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id){
+            if(id == null) return NotFound();
+            var productDTO = await _prodService.GetById(id);
+
+            if(productDTO == null) return NotFound();
+
+            var cat = await _catService.GetCategories();
+            ViewBag.Categories = new SelectList(cat, "Id", "Name", productDTO.CategoryId);
+
+            return productDTO != null? View(productDTO) : NotFound();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(ProductDTO prod){
+            if(ModelState.IsValid) {
+                try {
+                    await _prodService.Update(prod);
+                }
+                catch(Exception ex) {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(prod);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id){
+            if(id == null) return NotFound();
+
+            var prodDTO = await _prodService.GetById(id);
+            return prodDTO != null? View(prodDTO) : NotFound();
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id) {
+            await _prodService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Details(int? id){
+            if(id == null)
+                return NotFound();
+            var prodDTO = await _prodService.GetById(id);
+
+            if(prodDTO == null)
+                return NotFound();
+
+            var wwwroot = _env.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + prodDTO.Image);
+            
+            Console.WriteLine(image);
+
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+
+            return View(prodDTO);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
