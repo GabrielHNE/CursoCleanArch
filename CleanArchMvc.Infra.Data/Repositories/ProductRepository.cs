f09d882c5192a2fe469856768fd4cebb658ed2b6
Gabriel Henrique <gabrielhnespindola@gmail.com>
2024-05-24 00:32:19 -0300

    refactor: incluido a categoria ao buscar produto

--- CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -22,7 +22,8 @@ public class ProductRepository : IProductRepository
 
     public async Task<Product> GetByIdAsync(int? id)
     {
-        return await _productContext.Products.FindAsync(id);
+        return await _productContext.Products.Include(c => c.Category)
+        .SingleOrDefaultAsync(p => p.Id == id);
     }
 
     public async Task<Product> GetProductCategoryAsync(int? id)
