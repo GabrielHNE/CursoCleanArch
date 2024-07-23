29e8d8cc48c030fda6d117b69512f4a98d6f39f4
Gabriel Henrique <gabrielhnespindola@gmail.com>
2024-07-23 01:16:45 -0300

    Finalização dos endpoints de Category

--- CleanArchMvc.WebAPI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebAPI/Controllers/CategoriesController.cs
@@ -25,4 +25,53 @@ public class CategoriesController : ControllerBase
 
         return Ok(categories);
     }
+
+    [HttpGet("{id:int}", Name = "GetCategory")]
+    public async Task<ActionResult<CategoryDTO>> Get(int id)
+    {
+        var cat = await _catService.GetById(id);
+        
+        if(cat == null)
+            return NotFound("Category not found");
+
+        return Ok(cat);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create([FromBody] CategoryDTO catDTO)
+    {
+        if(catDTO == null)
+            return BadRequest("Invalid data");
+
+        await _catService.Add(catDTO);
+
+        return new CreatedAtRouteResult("GetCategory", new { id = catDTO.Id }, catDTO);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Update(int id, [FromBody] CategoryDTO categoryDTO)
+    {
+        if(id != categoryDTO.Id)
+            return BadRequest();
+
+        if(categoryDTO == null)
+            return BadRequest();
+
+        await _catService.Update(categoryDTO);
+
+        return Ok(categoryDTO);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<CategoryDTO>> Delete(int id)
+    {
+        var cat = _catService.GetById(id);
+
+        if(cat == null)
+            return NotFound("Category not found");
+
+        await _catService.Remove(id);
+
+        return Ok(cat);
+    }
 }
